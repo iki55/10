2bbbdf3ee525faaf618d56c57b35663c0fca92c2
iki55 <aleksovska.isidora@gmail.com>
2023-11-27 21:07:06 +0100

    Add first java program

---
+++ src/proekt_14/Sumpabr.java
@@ -0,0 +1,59 @@
+package proekt_14;
+
+import java.util.Scanner;
+
+public class Sumpabr {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+	    int n,sumpa=0;
+	    System.out.println ("Progrmaot presmetuva suma na parni broevi od dadena niza");
+		System.out.println ("Vnesete ja nizata na broevi");
+		n=input.nextInt();
+		int length = input.nextInt();
+        int[] sequence = new int[length];
+    	for(int i=0;i<n;i++) {
+        sequence[i] = input.nextInt();
+        System.out.println(i);
+		if (n%2==0 && n>=i);
+	    sumpa+=i;
+	    System.out.print(sequence[i] + " ");}
+	    System.out.println ("Sumata na parnite broevi iznesuva"+sumpa);
+		}			
+    	}
+		
+	
+		
+	
+		
+		
+		
+		
+		
+		
+	
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+		
+			
+				
+		
+		
+		
+    
+
+	
+
+
